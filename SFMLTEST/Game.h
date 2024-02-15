d005b240cbbad18fff56e5013c4d45aa8a5dd7fe
SeanWhelan117 <57904168+SeanWhelan117@users.noreply.github.com>
2024-02-15 14:44:09 +0000

    Added starter project

---
+++ SFMLTEST/Game.h
@@ -0,0 +1,36 @@
+/// <summary>
+/// author Sean Whelan 2024
+/// </summary>
+#ifndef GAME_HPP
+#define GAME_HPP
+/// <summary>
+/// include guards used so we don't process this file twice
+/// same as #pragma once
+/// Don't forget the endif at the bottom
+/// </summary>
+#include <SFML/Graphics.hpp>
+
+class Game
+{
+public:
+	Game();
+	~Game();
+	/// <summary>
+	/// main method for game
+	/// </summary>
+	void run();
+
+private:
+
+	void processEvents();
+	void processKeys(sf::Event t_event);
+	void update(sf::Time t_deltaTime);
+	void render();
+
+
+	sf::RenderWindow m_window; // main SFML window
+	bool m_exitGame; // control exiting game
+
+};
+
+#endif // !GAME_HPP
